96b34a180818aab5267f6f9e85fc3e9e66c702f5
assaadaladas <assaadaladas@hotmail.com>
2025-07-23 21:18:15 +0300

    Add project files.

---
+++ GarageService.GarageApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace GarageService.GarageApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ GarageService.GarageApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageService.GarageApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
