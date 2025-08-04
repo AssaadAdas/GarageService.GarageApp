b8cfbf0dfa3866d6ff9a571a998b15d1ae35eec4
assaadaladas <assaadaladas@hotmail.com>
2025-08-04 23:17:54 +0300

    update navigation for dashboard

--- GarageService.GarageApp/Views/GarageDashboardPage.xaml.cs
+++ GarageService.GarageApp/Views/GarageDashboardPage.xaml.cs
@@ -1,3 +1,4 @@
+using GarageService.GarageApp.Services;
 using GarageService.GarageApp.ViewModels;
 
 namespace GarageService.GarageApp.Views;
@@ -9,5 +10,15 @@ public partial class GarageDashboardPage : ContentPage
 		InitializeComponent();
         BindingContext = viewModel;
 
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (BindingContext is GarageDashboardViewModel vm)
+        {
+            await vm.LoadGarageProfile(); // Make sure this method fetches the latest profile
+        }
+        var navService = Handler.MauiContext.Services.GetService<INavigationService>();
+        (navService as NavigationService)?.SetCurrentPage(this);
     }
 }
